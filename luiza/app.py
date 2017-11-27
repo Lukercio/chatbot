408efde9a5e0784c5b493ff947a9d722b29093bf
Lukercio <lukercio@live.com>
2017-11-27 02:54:00 -0200

    heroku config

--- luiza/app.py
+++ luiza/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -31,4 +32,5 @@ def recebe_msg():
 	return jsonify({'OK': request.json}), 201
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
