ecb6f92a118a00eda265d9f0601bc7b9d9645ff5
Lukercio <lukercio@live.com>
2017-12-04 00:12:48 -0200

    get webhook

--- luiza/app.py
+++ luiza/app.py
@@ -21,44 +21,35 @@ db = connection[DB_NAME]
 db.authenticate(DB_USER, DB_PASS)
 
 token = 'a1s2d3f4'
-#def bot():
-	#print 'olar'
+
 	
-
-@app.route('/luizalabs/get-test', methods=['GET'])
-def get_test():
-	statusCode = 200
-
-	#if request.args.get('hub.verify_token'):
-	return request.args.get('hub.challenge'), 200
-
-	#return "Wrong Verify Token"
-
-	#retorno = db.log.find({})
-	#print retorno
-	#return 'Sucesso', statusCode
-	#return json.dumps(retorno, default=json_util.default), statusCode
-
-@app.route('/luizalabs/mensagens', methods=['POST'])
+@app.route('/luizalabs/mensagens', methods=['GET','POST'])
 def recebe_msg():
-	print('inicio metodo post')
-	statusCode = 200
-	retorno = 'Sucesso'
+	if request.method == 'GET':
+		if request.args.get('hub.verify_token') == token:
+			return request.args.get('hub.challenge'), 200
 
-	if not request.json or not 'title' in request.json:
-		abort(400)
+		return "Token incorreto"
 
-	try:
-		r = db.log.insert(request.json)
-	except:
-		retorno = 'Erro'
-		statusCode = 500
+	elif request.method == 'POST':
+		print('inicio metodo post')
+		statusCode = 200
+		retorno = 'Sucesso'
 
-	return retorno, statusCode
-	#json.dumps(r, default=json_util.default), statusCode
-	#return json.dumps(restaurantes, sort_keys=True, indent=4, default=json_util.default), 200
+		if not request.json or not 'title' in request.json:
+			abort(400)
+
+		try:
+			r = db.log.insert(request.json)
+		except:
+			retorno = 'Erro'
+			statusCode = 500
+
+		return retorno, statusCode
+		#json.dumps(r, default=json_util.default), statusCode
+		#return json.dumps(restaurantes, sort_keys=True, indent=4, default=json_util.default), 200
 
 
-if __name__ == '__main__':
-	port = int(os.environ.get("PORT", 5000))
-	app.run(debug=True, host='0.0.0.0', port=port)
+	if __name__ == '__main__':
+		port = int(os.environ.get("PORT", 5000))
+		app.run(debug=True, host='0.0.0.0', port=port)
